1f6201a035c55befbf1e2d4a8a1c420ee2a6a1d7
kidestw <kidestworku2321@gmail.com>
2023-10-28 02:29:57 +0300

    updated dev environment

--- backend/app.js
+++ backend/app.js
@@ -52,17 +52,11 @@ app.use("/api", jobRoute);
 
 __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running....");
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 // error middleware
 app.use(errorHandler);
