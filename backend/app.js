93e7dfec5f1acc3ea1f281ff62b9151a1f994d56
kidestw <85937676+kidestw@users.noreply.github.com>
2023-10-28 04:02:49 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -52,7 +52,7 @@ app.use("/api", jobRoute);
 
 __dirname = path.resolve();
 
-app.use(express.static(path.join(__dirname, "/frontend/index.html")));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
